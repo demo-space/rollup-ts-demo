6c593b328e2d791477d892412d71cc396d11aa30
lessfish <lessfish@outlook.com>
2019-06-13 11:19:57 +0800

    remove babel，ts 编译可以代替 babel 的功能

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel'
 import { uglify } from "rollup-plugin-uglify"
 import typescript from 'rollup-plugin-typescript2';
 
@@ -17,9 +16,6 @@ export default {
       include: 'node_modules/**', // Default: undefined
     }),
     typescript(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    }),
     uglify()
   ]
 }
